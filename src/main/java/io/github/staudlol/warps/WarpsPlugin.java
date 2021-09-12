0371becb88f333d6eee4b8108936183b1416b81c
Staud <77198963+staudlol@users.noreply.github.com>
2021-09-12 02:29:10 +0100

    first commit

---
+++ src/main/java/io/github/staudlol/warps/WarpsPlugin.java
@@ -0,0 +1,18 @@
+package io.github.staudlol.warps;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public class WarpsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
